a98952b86cd1a8bd791e24835061699bc88c322f
Matias Insaurralde <matias@insaurral.de>
2016-02-21 09:10:16 -0300

    Logger options

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :mandioca, Mandioca.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+  config :logger, :console, format: "[$level] $message\n"
+# config :logger, level: :error
 
 # Set a higher stacktrace during development.
 # Do not configure such in production as keeping
